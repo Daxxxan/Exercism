c0a8a340f4d577dea985edb132ccac717ce4f21c
Daxxxan <alex-bad97@hotmail.fr>
2019-09-02 16:45:40 +0200

    hamming implem

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,31 @@
+class Hamming {
+
+    private String leftStrand;
+    private String rightStrand;
+
+    Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.isEmpty() && !rightStrand.isEmpty()){
+            throw new IllegalArgumentException("left strand must not be empty.");
+        } else if (rightStrand.isEmpty() && !leftStrand.isEmpty()){
+            throw new IllegalArgumentException("right strand must not be empty.");
+        } else if(leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        } else {
+            this.leftStrand = leftStrand;
+            this.rightStrand = rightStrand;
+        }
+    }
+
+    int getHammingDistance() {
+        int hamming = 0;
+
+        for(int i = 0; i < leftStrand.length(); i++) {
+            if(leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                hamming++;
+            }
+        }
+
+        return hamming;
+    }
+
+}
