19a3f7834d0321187565a5f934980f5f9b5a3c4e
Unknown <alex-bad97@hotmail.fr>
2019-09-02 17:51:30 +0200

    amstrong refacto

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -3,12 +3,12 @@ import java.util.logging.Logger;
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
-		int result = 0;
 		String number = String.valueOf(numberToCheck);
 
-		for(int i = 0; i < number.length(); i++) {
-			result += (int)Math.pow(number.charAt(i) - '0', (int)number.length());
-		}
+		int result = number.chars()
+				.map(num -> num - '0')
+				.map(num -> (int)Math.pow(num, number.length()))
+				.sum();
 
 		return result == numberToCheck;
 	}
