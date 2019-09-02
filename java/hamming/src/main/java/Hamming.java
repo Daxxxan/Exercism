799a901b01d711b1b2bf15872fb4fa6684978152
Unknown <alex-bad97@hotmail.fr>
2019-09-02 17:57:57 +0200

    hamming refacto

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -6,14 +6,18 @@ class Hamming {
     Hamming(String leftStrand, String rightStrand) {
         if (leftStrand.isEmpty() && !rightStrand.isEmpty()){
             throw new IllegalArgumentException("left strand must not be empty.");
-        } else if (rightStrand.isEmpty() && !leftStrand.isEmpty()){
-            throw new IllegalArgumentException("right strand must not be empty.");
-        } else if(leftStrand.length() != rightStrand.length()) {
-            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-        } else {
-            this.leftStrand = leftStrand;
-            this.rightStrand = rightStrand;
         }
+
+        if (rightStrand.isEmpty() && !leftStrand.isEmpty()){
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+
+        if(leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
 
     int getHammingDistance() {
