b9b8d1b602c006419ea3fdb1dbda49c109e8fa20
Unknown <alex-bad97@hotmail.fr>
2019-09-08 12:14:08 +0200

    pangram refacto

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -2,16 +2,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PangramChecker {
+    private final static int ALPHABET = 26;
 
     public boolean isPangram(String input) {
-        ArrayList<String> alphabet = new ArrayList<>(Arrays.asList("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")));
-        input = input.toLowerCase();
-
-        for(int i = 0; i < input.length(); i++) {
-            alphabet.remove(String.valueOf(input.charAt(i)));
-        }
-
-        return alphabet.isEmpty();
+        return input.toLowerCase().chars()
+                .filter(Character::isAlphabetic)
+                .distinct()
+                .count() == ALPHABET;
     }
 
 }
