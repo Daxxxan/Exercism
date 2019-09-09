489ffe6d3c90094165b323ce45d047b5335d45e3
Unknown <alex-bad97@hotmail.fr>
2019-09-09 15:20:44 +0200

    space-age refacto

--- java/space-age/src/main/java/SpaceAge.java
+++ java/space-age/src/main/java/SpaceAge.java
@@ -3,6 +3,23 @@ class SpaceAge {
     private double seconds;
     private static final double EARTH_PERIOD = 365.25;
 
+    private enum Planet {
+        MERCURY(0.2408467),
+        VENUS(0.61519726),
+        EARTH(1.),
+        MARS(1.8808158),
+        JUPITER(11.862615),
+        SATURN(29.447498),
+        URANUS(84.016846),
+        NEPTUNE(164.79132);
+
+        private double earthYear;
+
+        Planet(double earthYear) {
+            this.earthYear = earthYear;
+        }
+    }
+
     SpaceAge(double seconds) {
         this.seconds = seconds;
     }
@@ -16,35 +33,35 @@ class SpaceAge {
     }
 
     double onEarth() {
-        return this.seconds / secondByYear(1);
+        return this.seconds / secondByYear(Planet.EARTH.earthYear);
     }
 
     double onMercury() {
-        return this.seconds / secondByYear(0.2408467);
+        return this.seconds / secondByYear(Planet.MERCURY.earthYear);
     }
 
     double onVenus() {
-        return this.seconds / secondByYear(0.61519726);
+        return this.seconds / secondByYear(Planet.VENUS.earthYear);
     }
 
     double onMars() {
-        return this.seconds / secondByYear(1.8808158);
+        return this.seconds / secondByYear(Planet.MARS.earthYear);
     }
 
     double onJupiter() {
-        return this.seconds / secondByYear(11.862615);
+        return this.seconds / secondByYear(Planet.JUPITER.earthYear);
     }
 
     double onSaturn() {
-        return this.seconds / secondByYear(29.447498);
+        return this.seconds / secondByYear(Planet.SATURN.earthYear);
     }
 
     double onUranus() {
-        return this.seconds / secondByYear(84.016846);
+        return this.seconds / secondByYear(Planet.URANUS.earthYear);
     }
 
     double onNeptune() {
-        return this.seconds / secondByYear(164.79132);
+        return this.seconds / secondByYear(Planet.NEPTUNE.earthYear);
     }
 
 }
