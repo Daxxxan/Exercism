a373fa751c9e5f90bd94842ca0150e148152abf7
Unknown <alex-bad97@hotmail.fr>
2019-09-29 12:18:13 +0200

    fsharp queen-attack implem

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,10 @@
+﻿module QueenAttack
+
+let create (x, y) =
+    let queenAxesWithinRange position = position > 0 && position < 8
+    queenAxesWithinRange x && queenAxesWithinRange y
+
+let canAttack (blackQueen: int * int) (whiteQueen: int * int) : bool =
+    let firstRow, firstColumn = blackQueen
+    let secondRow, secondColumn = whiteQueen
+    firstRow = secondRow || firstColumn = secondColumn || abs(firstRow - secondRow) = abs (firstColumn - secondColumn)
