0a2c8a2aaab6f38ddb1359e41ee80cf030907b27
Daxxxan <alex-bad97@hotmail.fr>
2019-10-17 20:26:20 +0200

    refacto

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -7,34 +7,18 @@ namespace HighScores
     {
         private List<int> _scores;
         
-        public HighScores(List<int> scores)
-        {
-            _scores = scores;
-        }
+        public HighScores(List<int> scores) => _scores = scores;
 
-        public List<int> Scores()
-        {
-            return _scores;
-        }
+        public List<int> Scores() => _scores;
 
-        public int Latest()
-        {
-            return _scores.Last();
-        }
+        public int Latest() => _scores.Last();
 
-        public int PersonalBest()
-        {
-            return _scores.Max();
-        }
+        public int PersonalBest() => _scores.Max();
 
-        public List<int> PersonalTopThree()
-        {
-            while (_scores.Count > 3)
-            {
-                _scores.Remove(_scores.Min());
-            }
-
-            return _scores.OrderByDescending(score => score).ToList();
-        }
+        public List<int> PersonalTopThree() => 
+            _scores
+                .OrderByDescending(score => score)
+                .Take(3)
+                .ToList();
     }
 }
