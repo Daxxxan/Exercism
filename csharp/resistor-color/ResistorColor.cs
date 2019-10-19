a635d0c7f6fb719207dc14982c560e69911e272f
Unknown <alex-bad97@hotmail.fr>
2019-10-19 16:13:43 +0200

    refacto

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -4,14 +4,10 @@ namespace ResistorColor
 {
     public static class ResistorColor
     {
-        public static int ColorCode(string color)
-        {
-            return Array.IndexOf(Colors(), color);
-        }
+        public static int ColorCode(string color) 
+            => Array.IndexOf(Colors(), color);
 
-        public static string[] Colors()
-        {
-            return new[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
-        }
+        public static string[] Colors() 
+            => new[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
     }
 }
