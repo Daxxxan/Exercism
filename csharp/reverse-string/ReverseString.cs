db42f5e89aa285887b5c795ee22b1c9677a22918
Unknown <alex-bad97@hotmail.fr>
2019-10-19 18:39:33 +0200

    csharp reverse string implem

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] result = input.ToCharArray();
+        Array.Reverse(result);
+        return new string(result);
+    }
+}
