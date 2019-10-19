b94890fd358370182c52fd0e02cf12f11f845e6c
Unknown <alex-bad97@hotmail.fr>
2019-10-19 19:08:43 +0200

    csharp synchronized bank account implem

---
+++ csharp/bank-account/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class BankAccount
+{
+    private decimal _balance;
+    private bool _open;
+
+    public BankAccount()
+    {
+        _open = true;
+    }
+    
+    public void Open()
+    {
+        _balance = 0;
+    }
+
+    public void Close()
+    {
+        _open = false;
+    }
+
+    public decimal Balance
+    {
+        get
+        {
+            if (_open)
+            {
+                return _balance;   
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+
+    public void UpdateBalance(decimal change)
+    {
+        lock (this)
+        {
+            _balance = decimal.Add(_balance, change);
+        }
+    }
+}
